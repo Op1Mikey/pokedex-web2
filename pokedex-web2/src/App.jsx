039d23f2ba107e6c340c84ec460b306b1b4f5e56
Op1Mikey <jordytsikiniaina@gmail.com>
2026-08-11 20:41:45 +0300

    Adding search bar to filter Pokemon by name

--- pokedex-web2/src/App.jsx
+++ pokedex-web2/src/App.jsx
@@ -5,12 +5,15 @@ import './App.css';
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(true);
+  //Un nouvel etat pour stocker ce que l'user tape
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     async function fetchPokemons() {
       // 1. On récupère la liste de base (noms + urls)
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0');
       const data = await response.json();
+
 
       // 2. Pour CHAQUE pokemon de la liste, on va chercher ses détails
       const detailedPokemons = await Promise.all(
@@ -31,11 +34,28 @@ function App() {
     return <p>Chargement des Pokémon...</p>;
   }
 
+  //filtrage des pokemons(variable contenant les pokemons filtres)
+  const filteredPokemons = pokemonList.filter((pokemon)=> 
+  pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <div className="App">
       <h1>Pokedex</h1>
+
+      <input 
+      type="text"
+      placeholder='Rechercher un Pokémon...'
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className='search-bar'
+       />
+
+       <p className="result-count">
+        {filteredPokemons.length} Pokémon{filteredPokemons.length > 1 ? 's' : ''}
+        trouvé{filteredPokemons.length > 1 ? 's' : ''}
+       </p>
       <div className="pokemon-grid">
-        {pokemonList.map((pokemon) => (
+        {filteredPokemons.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>
